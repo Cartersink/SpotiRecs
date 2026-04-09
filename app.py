9e11dca88af3288cdaf691f4d5b9fa656015647e
Cartersink <carterwensink@gmail.com>
2026-04-09 19:33:32 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+st.title("Song Recommender")
+
+df = pd.read_csv("tracks_features_clean.csv")
+
+query = st.text_input("Search songs, artists...")
+
+def recommend_songs(index):
+    songs = pd.read_csv("tracks_features.csv")
+    features = [
+    "danceability",
+    "energy",
+    "key",
+    "loudness",
+    "mode",
+    "speechiness",
+    "acousticness",
+    "instrumentalness",
+    "liveness",
+    "valence",
+    "tempo"
+    ]
+
+    X = songs[features]
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X) 
+    
+    knn = NearestNeighbors(
+    n_neighbors=6,   # 5 recommendations + itself
+    metric="cosine"
+    )
+
+    knn.fit(X_scaled)
+    
+    query_vector = X_scaled[index].reshape(1, -1)
+    distances, indices = knn.kneighbors(query_vector)
+    
+    recommended_indices = indices[0][0:]
+
+    recommendations = df.iloc[recommended_indices][
+        ["name", "artists"]
+    ]
+    
+    return recommendations
+
+
+if query:
+    matches = df[
+        df["name"].str.contains(query, case=False, na=False) |
+        df["artists"].str.contains(query, case=False, na=False)
+    ].copy()
+
+    matches["display_name"] = (
+        matches["name"] + " - " + matches["artists"]
+    )
+
+    matches = matches.drop_duplicates(subset=["display_name"])
+
+    if not matches.empty:
+        song_options = {
+            row["display_name"]: idx
+            for idx, row in matches.iterrows()
+        }
+
+        selected_song = st.selectbox(
+            "Choose a song",
+            list(song_options.keys())
+        )
+
+        selected_index = song_options[selected_song]
+
+        st.write("Selected index:", selected_index)
+        
+        if st.button("🎵 Recommend Songs"):
+            recommendations = recommend_songs(selected_index)
+
+            st.subheader("Recommended Songs")
+            st.dataframe(recommendations)       
+
+
+
